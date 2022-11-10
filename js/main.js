f5f865c8dba4374c8117f5528cf1873a3a75beef
damo25 <c2003739@hallam.shu.ac.uk>
2022-11-10 16:49:24 +0000

    java added

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+(function (){
+    //start
+    
+    document.getElementById("myHeading").innerHTML = "damo"
+
+    document.querySelector("nav ul li").setAttribute("class", "currentPage")
+
+    
+     //end
+})()   
