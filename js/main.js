766471fc9e44714c83e224a27a35e9edc9f89cae
damo25 <c2003739@hallam.shu.ac.uk>
2022-11-10 17:09:00 +0000

    jsUpdate

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@
 
     document.querySelector("nav ul li").setAttribute("class", "currentPage")
 
-    
+    let myFirstLink = document.querySelector("nav ul li a")
+    myFirstLink.setAttribute("a href=","https://www.google.com")
+
+    let myBtn = document.getElementById("myTestEvent")
+    myBtn.addEventListener("click", function(){
+        console.info("i was clicked")
+    })
+
      //end
 })()   
